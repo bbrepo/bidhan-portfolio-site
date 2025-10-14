1052c171ff7cef3e00e83c88254f2868f393db2d
HamimSaadAlRaji <hamimsaad.raji@gmail.com>
2025-10-14 11:15:01 +0600

    feat(summary): add summary section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Navbar from "./components/navbar";
 import Hero from "./sections/hero";
+import Summary from "./sections/Summary";
 import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="">
+      <div className="bg-gray-100">
         <Navbar />
         <main>
           <Hero />
+          <Summary />
         </main>
       </div>
     </BrowserRouter>
---
+++ src/sections/summary.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Summary = () => {
+  return (
+    <div className="">
+      <div className="flex justify-center md:justify-start mb-4 px-4 mt-4">
+        <h1 className="text-3xl md:text-4xl font-bold tracking-tight">
+          Career Summary
+        </h1>
+      </div>
+      <div className="w-full bg-gray-400/40">
+        <p className="text-gray-700 leading-relaxed text-justify p-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+          mollis neque, ut euismod nisl. Maecenas feugiat condimentum lectus,
+          quis molestie dolor dignissim sed. Pellentesque habitant morbi
+          tristique senectus et netus et malesuada fames ac turpis egestas. Sed
+          pulvinar quam a vestibulum vehicula. Quisque in dui est. Suspendisse
+          fermentum blandit velit, eu posuere leo egestas sed. Cras auctor
+          gravida urna, cursus porttitor magna ullamcorper nec. Cras nec
+          pulvinar nisl. Nunc mi urna, euismod eu ipsum nec, lobortis maximus
+          enim. In hac habitasse platea dictumst. Phasellus cursus enim iaculis,
+          convallis neque quis, scelerisque est. Suspendisse ac pretium metus.
+          Donec mi nisl, pulvinar eu pharetra sit amet, iacullis sed risus.
+          Curabitur ac ex vel lectus tempor tempus ut sed leo. Pellentesque arcu
+          lorem, consectetur et bibendum eu, fringilla at diam. Etiam et massa
+          tellus.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Summary;
